5ccf78002d7c0d497c059584de13b0b0283cfebd
AkhonaRSA <Akhonakhaya@gmail.com>
2024-08-13 09:28:53 +0200

    react_table

---
+++ src/components/tableRows.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../App.css';
+
+
+const TableRows = ({ data }) => {
+    return (
+        <tr>
+            <td>{data.roomNo}</td>
+            <td>{data.name}</td>
+            <td>{data.surname}</td>
+            <td>{data.cellNumber}</td>
+            <td>{data.appointmentDateTime}</td>
+            <td>{data.specialty}</td>
+            <td>{data.designation}</td>
+            <td className={data.status === 'In-progress' ? 'status in-progress' : 'status available'}>
+                {data.status}
+            </td>
+        </tr>
+    );
+};
+
+export default TableRows;
